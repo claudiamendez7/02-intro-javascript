f17b38aecb92588e9a403b7b62fb7ae09887905a
ClauMndz <claudiamendezt19@gmail.com>
2023-08-04 16:58:25 -0500

    importaciones y exportaciones

---
+++ src/bases/08-imp-ext.js
@@ -0,0 +1,24 @@
+
+// import {heroes} from './data/heroes'
+import heroes, {owners} from '../data/heroes';
+console.log(owners);
+
+
+
+// const getHeroebyId = (id) => {
+//     return heroes.find ((heroe) => {
+//         if (heroe.id === id){
+//             return true;
+//         else {
+//             return false;
+//         }
+//         }
+//     });
+// }
+
+const getHeroeById = (id) => heroes.find ((heroe) => heroe.id === id);
+
+console.log(getHeroeById(2));
+
+const getHeroesByOwner = (owner) => heroes.filter ((heroe) => heroe.owner === owner);
+console.log(getHeroesByOwner('Marvel'));
