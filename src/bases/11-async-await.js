e496349ca7f913f58a27ae602c201fb3cf5a9683
ClauMndz <claudiamendezt19@gmail.com>
2023-08-09 09:46:05 -0500

    Async Await

---
+++ src/bases/11-async-await.js
@@ -0,0 +1,24 @@
+
+
+// const getImagenPromesa = () => new Promise(resolve=>'http://skjfbjkb.com'); 
+// getImagenPromesa().then (console.log);
+
+const getImagen = async() => {
+
+    try{
+        const apiKey = 'lr5CXYM2cwhyyPbaQk9TpWloUfo0If9f';
+        const resp =await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+        const {data} = await resp.json();
+
+        const {url} = data.images.original;
+        
+        const img =document.createElement('img');
+        img.src = url;
+        document.body.append(img);
+    }catch(error) {
+        // manejo del error
+        console.error(error)
+    }
+}
+
+getImagen();
